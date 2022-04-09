5d5786e0467b68178fb7e5ffa26458cdd8f60cc9
Subtawee Hanyut <subtawee.h@gmail.com>
2022-04-09 12:12:35 +0700

    swagger

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const connectDB = require("./config/db");
 const hospitals = require("./routes/hospitals");
 const auth = require("./routes/auth");
 const appointments = require("./routes/appointments");
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 connectDB();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -24,13 +28,14 @@ const limiter = rateLimit({
   windowsMs: 10 * 60 * 1000, //10 mins
   max: 1,
 });
-app.use(limiter);
+// app.use(limiter);
 app.use(hpp());
 app.use(cors());
 app.use(cookieParser());
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/appointments", appointments);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -38,8 +43,6 @@ app.get("/", (req, res) => {
     data: { id: 1 },
   });
 });
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(`🚀🚀🚀🚀🚀 Server started on port ${PORT} 🚀🚀🚀🚀🚀`)
